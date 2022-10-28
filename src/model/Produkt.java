39d00629e073743a35765b4d1668602da2ffff7a
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-10-28 10:59:24 +0200

    Lavet Produkt

--- src/model/Produkt.java
+++ src/model/Produkt.java
@@ -1,4 +1,23 @@
 package model;
 
 public class Produkt {
+    private String navn;
+    private String beskrivelse;
+    // -------------------------------------------------------------------
+    public Produkt(String navn, String beskrivelse) {
+        this.navn = navn;
+        this.beskrivelse = beskrivelse;
+    }
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+    public String getNavn() {
+        return navn;
+    }
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
 }
