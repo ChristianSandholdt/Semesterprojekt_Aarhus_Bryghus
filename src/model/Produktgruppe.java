11845e53a8650b797d5aa4419bae4e8d0f6485e3
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-10-28 12:15:24 +0200

    Lavet Produkt

--- src/model/Produktgruppe.java
+++ src/model/Produktgruppe.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Produktgruppe {
+    private String navn;
+    private String type;
+
+    public Produktgruppe(String navn, String type) {
+        this.navn = navn;
+        this.type = type;
+    }
+    // ----------------------------------------------------------------------------
+
+
+
 }
