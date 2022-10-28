6e3fcf3598cf07ae4bd72c0692b2ee21871c5484
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-10-28 10:39:24 +0200

    Lavet kunde

--- src/model/Kunde.java
+++ src/model/Kunde.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 
 public class Kunde {
 
+    // Association -> 0..x Ordre
+    private final ArrayList<Ordre> ordrer = new ArrayList<>();
+
+    public ArrayList<Ordre> getOrdrer(){
+        return new ArrayList<>(ordrer);
+    }
     private final String name;
     private final String email;
     private final int telefonNr;
     private int antalKlip;
     private boolean studerende;
-
-
 
     public Kunde(String name, String email, int telefonNr, int antalKlip, boolean studerende) {
         this.name = name;
@@ -19,8 +23,19 @@ public class Kunde {
         this.antalKlip = antalKlip;
         this.studerende = studerende;
     }
+    // ---------------------------------------------------------------------------------------
 
-    public ArrayList<Integer> koebsHistorik = new ArrayList<>();
+    /**
+     * Checks whether the customer qualifies for a discount based on number of orders
+     * Returns a boolean either true or false
+     */
+    public boolean godKunde(){
+        boolean god = false;
+        if (getOrdrer().size() >= 5){
+            god = true;
+        }
+        return god;
+    }
 
 
 }
