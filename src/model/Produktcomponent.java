3c7546c74089c8a32ad0c500f0bfc232c440a7c4
Rosell22V <Gustav@klitholm.com>
2022-10-28 11:52:34 +0200

    siuuu
    rundvisning og ordrelinje

--- src/model/Produktcomponent.java
+++ src/model/Produktcomponent.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Produktcomponent {
+
+    private String navn;
+
+    // ------------------------------------------------------------------------------------------
+
+
+    public Produktcomponent(String navn) {
+        this.navn = navn;
+    }
+
+    // ------------------------------------------------------------------------------------------
+
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    // ------------------------------------------------------------------------------------------
+
+
 }
--- src/model/Produktgruppe.java
+++ src/model/Produktgruppe.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Produktgruppe {
+
+    private String navn;
+    private String type;
+
+
+    // ------------------------------------------------------------------------------------------
+
+
+    public Produktgruppe(String navn, String type) {
+        this.navn = navn;
+        this.type = type;
+    }
+
+    // ------------------------------------------------------------------------------------------
+
+
+
 }
