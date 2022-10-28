63fe9c3d45fa52045865e8cbd9258282d403020f
Puffbirbb <eaagmhc@students.eaaa.dk>
2022-10-28 10:28:23 +0200

    mit lort er jo fucked

--- src/model/Ordre.java
+++ src/model/Ordre.java
@@ -1,4 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Ordre {
+    public boolean betalt;
+    public int sum;
+    public String betalingsform;
+    public String ordreID;
+
+    public Ordre(boolean betalt, int sum, String betalingsform, String ordreID) {
+        this.betalt = betalt;
+        this.sum = sum;
+        this.betalingsform = betalingsform;
+        this.ordreID = ordreID;
+    }
+
+    // Aggregation -> 0..*
+    private final ArrayList<Ordrelinje> ordrelines = new ArrayList<>();
+
+    public ArrayList<Ordrelinje> getOrdrelinjes() {
+        return new ArrayList<>(ordrelines);
+    }
+
+    public Ordrelinje createOrderLine(int antal, Produkt produkt){
+        OrderLinje
+    }
 }
