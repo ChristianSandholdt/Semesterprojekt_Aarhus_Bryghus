2389e1a1b37878bf3d8846b1ad2f31c729c7ed79
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-10-31 12:44:09 +0100

    Lavet Produktwindow

--- src/gui/Produktwindow.java
+++ src/gui/Produktwindow.java
@@ -2,12 +2,14 @@ package gui;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.Produkt;
 import model.Produktgruppe;
 
 public class Produktwindow extends Stage{
@@ -33,6 +35,8 @@ public class Produktwindow extends Stage{
 
     }
     private final ListView<Produktgruppe> lvwProduktGruppe = new ListView<>();
+    private final ListView<Produkt> lvwProdukt = new ListView<>();
+
     private void initContent(GridPane pane){
         pane.setGridLinesVisible(false);
         pane.setPadding(new Insets(20));
@@ -42,8 +46,28 @@ public class Produktwindow extends Stage{
         Label lblProduktGruppe = new Label("Produktgrupper");
         pane.add(lblProduktGruppe,0,0);
 
+        Label lblProdukt = new Label("Produkter");
+        pane.add(lblProdukt,1,0);
+
         pane.add(lvwProduktGruppe,0,1);
+        pane.add(lvwProdukt,1,1);
+
+        //Tilføj ny produktgruppe
+        Button btnTilfoejProduktGruppe = new Button("Opret ny produktgruppe");
+        pane.add(btnTilfoejProduktGruppe,0,2);
 
 
+        //Tilføj nyt produkt
+        Button btnTilfoejProdukt = new Button("Tilføj nyt produkt");
+        pane.add(btnTilfoejProdukt,1,2);
+        //Fjern produkt
+        Button btnFjernProdukt = new Button("Fjern produkt");
+        pane.add(btnFjernProdukt,1,3);
+
+        //Rediger produkt
+        Button btnRedigerProdukt = new Button("Rediger produkt");
+        pane.add(btnRedigerProdukt,1,4);
     }
+
+
 }
