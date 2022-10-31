35810da5fa9efe4615c044b61ae472e5be7ae4fd
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-10-31 11:08:52 +0100

    Lavet Produkt

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -5,6 +5,7 @@ import controller.Controller;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import storage.ListStorage;
@@ -20,6 +21,8 @@ public class Gui extends Application {
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
+
+        produktWindow = new Produktwindow("Produkter", stage);
     }
 
     @Override
@@ -28,10 +31,42 @@ public class Gui extends Application {
     }
     //--------------------------------------------------------------------------------
 
+    private Produktwindow produktWindow;
+
     private void initContent(GridPane pane) {
         pane.setGridLinesVisible(false);
         pane.setPadding(new Insets(20));
         pane.setHgap(10);
         pane.setVgap(10);
+
+        //Produkt
+        Button btnProdukt = new Button("Produkter");
+        btnProdukt.setMinHeight(100);
+        btnProdukt.setMinWidth(120);
+        pane.add(btnProdukt,0,0);
+
+        //Ordre
+        Button btnOrdre = new Button("Ny Ordre");
+        btnOrdre.setMinHeight(100);
+        btnOrdre.setMinWidth(120);
+        pane.add(btnOrdre,1,0);
+
+        //Statistik
+        Button btnStatistik = new Button("Statistik");
+        btnStatistik.setMinHeight(100);
+        btnStatistik.setMinWidth(120);
+        pane.add(btnStatistik,1,1);
+
+        //Rundvisning
+        Button btnRundvisning = new Button("Book Rundvisning");
+        btnRundvisning.setMinHeight(100);
+        btnRundvisning.setMinWidth(120);
+        pane.add(btnRundvisning,0,1);
     }
+
+    private void btnProduktAction(){
+        produktWindow.
+    }
+
+
 }
