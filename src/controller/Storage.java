7525b485e7423cc6c90cf53a2b299e642d80f53f
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-11-01 09:42:01 +0100

    Produktgruppe

--- src/controller/Storage.java
+++ src/controller/Storage.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface Storage {
 
     //--------------------------------------------------------------------------------------------------
-    List<Produktgruppe> getProduktgrupper();
+    List<Produktgruppe> getProduktgruppe();
     void storeProduktgruppe(Produktgruppe produktgruppe);
     void deleteProduktgruppe(Produktgruppe produktgruppe);
     //--------------------------------------------------------------------------------------------------
