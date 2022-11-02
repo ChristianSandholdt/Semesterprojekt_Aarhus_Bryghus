84e7963e82078f163012439222ef2d521ea41817
Puffbirbb <eaagmhc@students.eaaa.dk>
2022-11-02 10:06:40 +0100

    mit lort er jo fucked

--- src/gui/RundvisningsWindow.java
+++ src/gui/RundvisningsWindow.java
@@ -1,9 +1,15 @@
 package gui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.skin.DatePickerSkin;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -22,9 +28,9 @@ public class RundvisningsWindow extends Stage {
         this.initOwner(owner);
         this.initStyle(StageStyle.UTILITY);
         this.initModality(Modality.APPLICATION_MODAL);
-        this.setMinHeight(100);
-        this.setMinWidth(200);
-        this.setResizable(false);
+        this.setMinHeight(500);
+        this.setMinWidth(500);
+        this.setResizable(true);
 
         this.setTitle(title);
         BorderPane pane = new BorderPane();
@@ -34,12 +40,26 @@ public class RundvisningsWindow extends Stage {
         this.setScene(scene);
     }
 
+    private final TextField txfReservation = new TextField();
+    private final Button btnReserver = new Button("Reserver");
     private void initContent(BorderPane pane) {
-        pane.setPadding(new Insets(20));
+        pane.setPadding(new Insets(40));
 
+        DatePicker datePicker = new DatePicker(LocalDate.now());
         DatePickerSkin datePickerSkin = new DatePickerSkin(new DatePicker(LocalDate.now()));
         Node popupContent = datePickerSkin.getPopupContent();
 
-        pane.setCenter(popupContent);
+        pane.setTop(popupContent);
+
+        Label lblDatoValgt = new Label("Reservation");
+        pane.setLeft(lblDatoValgt);
+
+        pane.setCenter(txfReservation);
+        txfReservation.setPrefWidth(500);
+        datePicker.valueProperty().addListener(
+                (observable, oldValue, newValue) ->
+                        System.out.println("" + oldValue + newValue));
+
+        pane.setRight(btnReserver);
     }
 }
