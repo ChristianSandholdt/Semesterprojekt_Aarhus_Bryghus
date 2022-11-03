b45600783b7424646edfbf3737c4134f4fc7f7bc
Rosell22V <Gustav@klitholm.com>
2022-11-03 11:32:14 +0100

    dd

--- src/gui/BetalingsWindow.java
+++ src/gui/BetalingsWindow.java
@@ -1,10 +1,14 @@
 package gui;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -27,10 +31,14 @@ public class BetalingsWindow extends Stage {
 
         Scene scene = new Scene(pane);
         this.setScene(scene);
+
     }
 
     // -------------------------------------------------------------------------
-    private final TextField txfSiu = new TextField();
+    private final TextField txfKortnummer = new TextField();
+    private final TextField txfMåned = new TextField();
+    private final TextField txfDato = new TextField();
+    private final TextField txfCVV = new TextField();
 
 
     private void initContent(GridPane pane) {
@@ -39,14 +47,45 @@ public class BetalingsWindow extends Stage {
         pane.setHgap(10);
         pane.setVgap(10);
 
-        // Siu
-        Label lblTitel = new Label("Siu");
-        pane.add(lblTitel, 0, 0);
+        // Kortnummer
+        Label lblKortnr = new Label("Kortnummer: ");
+        pane.add(lblKortnr, 0, 0);
+        pane.add(txfKortnummer, 0, 1,2,1);
+        txfKortnummer.setMaxWidth(400);
 
 
+        // Udløbsdato
+        Label lblUdløbsdato = new Label("Udløbsdato: ");
+        pane.add(lblUdløbsdato, 0, 2);
+        HBox hbox1 = new HBox(5,txfMåned,txfDato);
+        hbox1.setAlignment(Pos.BASELINE_LEFT);
+        hbox1.setMaxWidth(75);
+        pane.add(hbox1, 0, 3);
+
+        // CVV
+        Label lblCVV = new Label("CVV:");
+        pane.add(lblCVV, 1, 2);
+        pane.add(txfCVV, 1, 3);
 
 
+        // Checkbox
+        Label lblStuderende = new Label("Studerende: ");
+        //pane.add(lblStuderende, 1,0);
+        CheckBox cbStuderende = new CheckBox();
+        //pane.add(cbStuderende, 1, 0);
+        //cbStuderende.setAlignment(Pos.CENTER)
+        HBox hbox2 = new HBox(5,lblStuderende,cbStuderende);
+        pane.add(hbox2, 1, 0);
 
+        // Betalingsknap
+        Button btnBetal = new Button("Betal");
+        pane.add(btnBetal, 0, 4);
+
+        // klippekort?
+        Label lblKlippekort = new Label("Klippekort");
+        CheckBox cbKlippekort = new CheckBox();
+        HBox hbox3 = new HBox(5,lblKlippekort,cbKlippekort);
+        pane.add(hbox3,   1, 4);
 
 
 
