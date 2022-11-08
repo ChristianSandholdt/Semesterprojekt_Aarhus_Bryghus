ee84f3f6ff9931d4edd90b9d6f67af04c3cf9807
Puffbirbb <eaagmhc@students.eaaa.dk>
2022-11-08 21:59:00 +0100

    Merge branch 'master' of https://github.com/ChristianSandholdt/Semesterprojekt_Aarhus_Bryghus
    # Conflicts:
    #	out/production/Semesterprojekt_Aarhus_Bryghus/gui/NyOrdreTab.class
    #	out/production/Semesterprojekt_Aarhus_Bryghus/gui/NyOrdreWindow.class
    #	out/production/Semesterprojekt_Aarhus_Bryghus/model/Ordrelinje.class

--- src/gui/NyOrdreTab.java
+++ src/gui/NyOrdreTab.java
@@ -133,8 +133,10 @@ public class NyOrdreTab extends GridPane {
         int ordreID = 1;
         int antal = Integer.parseInt(txfAntal.getText().trim());
         Produkt produkt = lvwProdukt.getSelectionModel().getSelectedItem();
-        ordre = Controller.createOrdre(false, ordreID, LocalDate.now());
-        ordreID++;
+        if (ordre == null){
+            ordre = Controller.createOrdre(false, ordreID, LocalDate.now());
+            ordreID++;
+        }
         pris = Controller.getPris(prisliste, produkt);
         ordrelinje = Controller.createOrdrelinje(antal, produkt, pris);
         ordre.addOrdrelinje(ordrelinje);
