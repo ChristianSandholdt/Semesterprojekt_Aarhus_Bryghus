0df1b636808779412d58c70d0a562306c8116d90
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-11-10 16:28:37 +0100

    .

--- src/gui/UdlejningsStatistikWindow.java
+++ src/gui/UdlejningsStatistikWindow.java
@@ -139,7 +139,5 @@ public class UdlejningsStatistikWindow extends Stage {
             alert.show();
         }
     }
-    void update(){
-        lvwUdlejninger.getItems().setAll(Controller.visUdlejninger());
-    }
+
 }
